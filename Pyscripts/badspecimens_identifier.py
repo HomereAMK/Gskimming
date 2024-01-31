aceaaced610e3fdffd0498fd1dad7059201e9035
Homère J. Alves Monteiro <35937197+HomereAMK@users.noreply.github.com>
2024-01-31 20:03:59 +0100

    new md

--- Pyscripts/badspecimens_identifier.py
+++ Pyscripts/badspecimens_identifier.py
@@ -1,43 +1,4 @@
-import csv
-import sys
 
-def process_csv_to_tsv(input_csv_file, output_tsv_file):
-    with open(input_csv_file, mode='r') as infile:
-        # Adjust the delimiter here if it's not a comma
-        reader = csv.reader(infile, delimiter='\t')
-
-        data_dict = {}
-
-        for row in reader:
-            # Skip rows that don't have exactly 3 columns
-            if len(row) != 3:
-                print(f"Skipping row: {row}")
-                continue
-
-            key, variable, value = row
-
-            if key not in data_dict:
-                data_dict[key] = {}
-            data_dict[key][variable] = value
-
-    with open(output_tsv_file, mode='w', newline='') as outfile:
-        writer = csv.writer(outfile, delimiter='\t')
-
-        for key, values in data_dict.items():
-            for variable, value in values.items():
-                writer.writerow([key, variable, value])
-
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python csv_to_tsv_stats.py <input_csv_file> <output_tsv_file>")
-        sys.exit(1)
-
-    input_csv_file = sys.argv[1]
-    output_tsv_file = sys.argv[2]
-
-    process_csv_to_tsv(input_csv_file, output_tsv_file)
-
-(tutorial) [sjr729@mjolnirhead01fl testClupea]$ cat badspecimens_identifier.py
 
 import sys
 import os
--- Rscripts/11janClupeaWranglin.R
+++ Rscripts/11janClupeaWranglin.R
@@ -8,16 +8,25 @@ rm(list=ls())
 # Loads required packages ~
 pacman::p_load(pheatmap, tidyverse, reshape2)
 
-help("read_csv")
+#### ClupeaAtmore ####
 # Reading the TSV file into a data frame
-data1 <- read_tsv("~/Desktop/GitHub/Gskimming/Metadata/Clupea/11janENA_clupeaAtmore.tsv")
+data1 <- read_tsv("~/Desktop/GitHub/Gskimming/Metadata/ClupeaAtmore/11janENA_clupeaAtmore.tsv")
 
 # Reading the CSV file into a data frame
-data2 <- read_delim("~/Desktop/GitHub/Gskimming/Metadata/Clupea/11janModern_Inds_Clupea.csv", delim = ";")
+data2 <- read_delim("~/Desktop/GitHub/Gskimming/Metadata/ClupeaAtmore/11janModern_Inds_Clupea.csv", delim = ";")
 
 # Merging the data frames on the 'sample_title' variable
 merged_data <- merge(data1, data2, by = "sample_title")
 
 # Viewing the first few rows of the merged data frame
 head(merged_data)
-write.csv(merged_data, "~/Desktop/GitHub/Gskimming/Metadata/Clupea/Master_list_ClupeaAtmore.csv", row.names = FALSE)
+write.csv(merged_data, "~/Desktop/GitHub/Gskimming/Metadata/ClupeaAtmore/Master_list_ClupeaAtmore.csv", row.names = FALSE)
+
+
+#### ClupeaHan ####
+# Reading the TSV file into a data frame
+Han_raw <- read_tsv("~/Desktop/GitHub/Gskimming/Metadata/ClupeaHan/filereport_read_run_PRJNA642736_ClupeaHan.tsv")
+
+# Reading the CSV file into a data frame
+data2 <- read_delim("~/Desktop/GitHub/Gskimming/Metadata/ClupeaAtmore/11janModern_Inds_Clupea.csv", delim = ";")
+
